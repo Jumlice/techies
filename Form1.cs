98c48928f23c6fedcae5c84ec6a24f8695cb4bf1
Jumlice <162418199+Jumlice@users.noreply.github.com>
2024-03-06 10:20:52 +0300

    second

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+﻿using Miner.Controllers;
+using System.Windows.Forms;
+
+namespace Techies
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MapController.Init(this);
+        }
+        
+        
+    }
+}
